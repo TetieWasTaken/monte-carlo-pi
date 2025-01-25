26d509a075b4ddf4fcbf2e5ce76e7b70cd556ff1
TetieWasTaken <tjvssr@gmail.com>
2025-01-25 09:04:32 +0100

    feat(content): add circle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,23 @@ export default function Home() {
       <p className="text-lg">
         Testing testing
       </p>
+      <svg width="500" height="500">
+        <rect
+          x="0"
+          y="0"
+          width="500"
+          height="500"
+          fill="none"
+          stroke="white"
+          strokeWidth="3"
+        />
+        <path
+          d="M 500 500 A 500 500 0 0 0 0 0"
+          fill="none"
+          stroke="white"
+          strokeWidth="3"
+        />
+      </svg>
     </div>
   );
 }
